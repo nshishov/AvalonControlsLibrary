aa9baf2d8a7513f050680e83bcf448abb800f816
Nikolay Shishov <nikolay.shishov@admiralmarkets.com>
2019-01-16 17:25:32 +0200

    Source code added

---
+++ AvalonControlsLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AvalonControlsLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marlon Grech")]
+[assembly: AssemblyProduct("AvalonControlsLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+
+[assembly: XmlnsDefinition("http://schemas.AvalonControls/AvalonControlsLibrary/Controls", "AC.AvalonControlsLibrary.Controls")]
+
+[assembly: InternalsVisibleTo("AvalonControlsLibraryVSTesting, PublicKey=0024000004800000940000000602000000240000525341310004000001000100cf05b7c9799c7be5e38559f3c92df3d61ff7158915651354556aa68ced875246a67387f07d7ce5bcc3c0b26d9a5de3a433f3f1d368f49e98a8860a124a89ecad112ae65db92077f4111e4ad758c9643e6e40a86d035667d627a8d6fabe82439d4aa1001619e3d88bade9c0fe66f7c8bab0381b968e17f8ce96b6c29bb2d47bcb")]
